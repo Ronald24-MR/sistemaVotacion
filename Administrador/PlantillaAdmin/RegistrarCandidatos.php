18e5b651befe4250869240fabc675f0e71c34ead
APRENDIZ <APRENDIZ@SINAPRRSCFSD559.FormacionSena.RED>
2022-09-19 11:54:27 -0500

    creacion

---
+++ Administrador/PlantillaAdmin/RegistrarCandidatos.php
@@ -0,0 +1,162 @@
+<?php include("menu.php") ?>
+
+<link rel="stylesheet" href="../../css/bootstrap.min.css">
+<link rel="stylesheet" href="css/styleCandidatos.css">
+
+<div class="container contenedor">
+<form action="RegistrarCandidatos.php" method="post" enctype="multipart/form-data">
+        <h2>Registro de candidatos</h2>
+        <div class="row">
+            <div class="col">
+                <select name="td" id="td" class="form-control" required>
+                    <option value="0" disabled selected hidden >Tipo de documento</option>
+                    <?php
+                        if(include('../../conectar.php'))
+                        {
+                            $sql="select * from tipo_documento";
+                            $tabla=mysqli_query($conectar,$sql);
+                            while($fila=mysqli_fetch_array($tabla))
+                            {
+                                print("<option value='$fila[0]'>");
+                                print($fila[1]);
+                                print("</option>");
+                            }
+                        }
+                        else
+                        {
+                            print("error al conectarse a la base de datos");
+                        }
+                    ?>
+                </select>
+            </div>
+            <div class="col">
+                <input type="number" class="form-control" name="nd" placeholder="Numero de documento" required>
+            </div>
+            <div class="col">
+                <input type="text" class="form-control" name="nombres" placeholder="Nombres" required>
+            </div >
+        </div>
+
+        <div class="row">
+            <div class="col">
+                <input type="text" class="form-control" name="apellidos" placeholder="Apellidos" required>
+            </div>
+            <div class="col">
+                <select name="formacion" id="formacion" class="form-control" required>
+                    <option value="0" disabled selected hidden>Formacion</option>
+                    <?php
+                        if(include('../../conectar.php'))
+                        {
+                            $sql="select * from formacion";
+                            $tabla=mysqli_query($conectar,$sql);
+                            while($fila=mysqli_fetch_array($tabla))
+                            {
+                                print("<option value='$fila[0]'>");
+                                print($fila[1]);
+                                print("</option>");
+                            }
+                        }
+                        else
+                        {
+                            print("error al conectarse a la base de datos");
+                        }
+                    ?>
+                </select>
+            </div>
+            <div class="col">
+                <select name="sede" id="sede" class="form-control" required>
+                    <option value="0" disabled selected hidden>Sede</option>
+                    <?php
+                        if(include('../../conectar.php'))
+                        {
+                            $sql="select * from sede";
+                            $tabla=mysqli_query($conectar,$sql);
+                            while($fila=mysqli_fetch_array($tabla))
+                            {
+                                print("<option value='$fila[0]'>");
+                                print($fila[1]);
+                                print("</option>");
+                            }
+                        }
+                        else
+                        {
+                            print("error al conectarse a la base de datos");
+                        }
+                    ?>
+                </select>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col">
+                <input type="file" class="form-control" name="fotografia" placeholder="fotografia" required>
+            </div>
+            
+            
+        </div>
+
+        <div class="row">
+        <div class="col">
+                <textarea name="pc" id="pc" cols="40" rows="5" placeholder="Escriba propuesta (maximo 300 caracteres)" class="form-control" required></textarea>
+            </div>
+        </div>
+        <div class="row">
+                
+                    <br>
+                <div class="col button"><button class="btn btn-primary" type="submit" name="boton" style="background-color: #212529; border:none;">Guardar</button></div>
+             
+            </div>
+   
+    </form>
+    </div>
+   
+    <?php
+    if(isset($_POST['boton']))
+    {
+        $td=$_POST['td'];
+        $nd=$_POST['nd'];
+        $nombres=$_POST['nombres'];
+        $apellidos=$_POST['apellidos'];
+        $formacion=$_POST['formacion'];
+        $sede=$_POST['sede'];
+        $nombrefoto=$_FILES['fotografia']['name'];
+        $tipofile=$_FILES['fotografia']['type'];
+        $pc=$_POST['pc'];
+        
+        $sql="insert into registrar_votaciones value($nd, $td, '$nombres', '$apellidos', $formacion, $sede,0,'$nombrefoto', '$pc', '$nd')";
+
+        if(!(strpos($tipofile,"jpg") || strpos($tipofile,"png") || strpos($tipofile,"jpeg")))
+        {
+            print("<script>Swal.fire('Tipo de archivo no valido')</script>");
+        }
+        else
+        {
+            if(include('../../conectar.php'))
+            {
+                if(mysqli_query($conectar,$sql))
+                {
+                    
+                    $ruta="fotos/".$nombrefoto;
+                    $sql="insert into usuario value($nd,'$nd','$nombres $apellidos',2,0)";
+                    mysqli_query($conectar,$sql);
+                    if (move_uploaded_file($_FILES['fotografia']['tmp_name'],$ruta))
+                    {
+                        print("<script>Swal.fire('Registro guardado correctamente')</script>");
+                    }
+                    else
+                    {
+                        print("<script>Swal.fire('Ocurrio algun error al subir el fichero. No pudo guardarse.')</script>");
+                    }
+                }
+                else
+                {
+                    print("<script>Swal.fire('Error al guardar los datos')</script>");
+                }
+            }
+        else
+        {
+            print("error al conectar");
+        }
+        }
+    }
+?>
---
+++ Administrador/PlantillaAdmin/menu.php
@@ -0,0 +1,87 @@
+    <?php include("cabecera.php"); ?>
+    <body class="sb-nav-fixed">
+    <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
+            <!-- Navbar Brand-->
+            <a class="navbar-brand ps-3" href="index.php">Dashboard</a>
+            <!-- Sidebar Toggle-->
+            <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
+            <!-- Navbar Search-->
+            
+            <!-- Navbar-->
+            <ul class="navbar-nav ms-auto me-0 me-md-3 my-2 my-md-0">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
+                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="#!">Settings</a></li>
+                        <li><a class="dropdown-item" href="#!">Activity Log</a></li>
+                        <li><hr class="dropdown-divider" /></li>
+                        <li><a class="dropdown-item" href="#!">Logout</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+        <div id="layoutSidenav">
+            <div id="layoutSidenav_nav">
+                <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+                    <div class="sb-sidenav-menu">
+                        <div class="nav">
+                            <div class="sb-sidenav-menu-heading">PRINCIPAL</div>
+                            <a class="nav-link" href="index.php">
+                                <div class="sb-nav-link-icon"></div>
+                                Dashboard
+                            </a>
+                            <a class="nav-link" href="">
+                                <div class="sb-nav-link-icon"></div>
+                                Pagina Votacion
+                            </a>
+                            <div class="sb-sidenav-menu-heading">FORMULARIOS</div>
+                            <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
+                                <div class="sb-nav-link-icon"></div>
+                                Gestionar Candidatos
+                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                            </a>
+                            <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                                <nav class="sb-sidenav-menu-nested nav">
+                                    <a class="nav-link" href="RegistrarCandidatos.php">Registrar</a>
+                                </nav>
+                            </div>
+                            <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
+                                <div class="sb-nav-link-icon"></div>
+                                Gestionar Votantes
+                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                            </a>
+                            
+                            <div class="collapse" id="collapsePages" aria-labelledby="headingTwo" data-bs-parent="#sidenavAccordion">
+                                <nav class="sb-sidenav-menu-nested nav accordion" id="sidenavAccordionPages">
+                                    <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#pagesCollapseAuth" aria-expanded="false" aria-controls="pagesCollapseAuth">
+                                        Authentication
+                                        <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                                    </a>
+                                    <div class="collapse" id="pagesCollapseAuth" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordionPages">
+                                        <nav class="sb-sidenav-menu-nested nav">
+                                            
+                                        </nav>
+                                    </div>
+                                    
+                                    
+                                </nav>
+                            </div>
+                            <div class="sb-sidenav-menu-heading">CRUD</div>
+                            
+                            <a class="nav-link" href="tables.php">
+                                <div class="sb-nav-link-icon"></div>
+                                Tablas
+                            </a>
+                        </div>
+                    </div>
+                    <div class="sb-sidenav-footer">
+                        <div class="small">Logged in as:</div>
+                        Start Bootstrap
+                    </div>
+                </nav>
+            </div>
+
+     
+    </body>
+
+    <?php include("footer.php"); ?>
